7db0146b76916346afd59ff359c3dd5af1078842
Brett Deaton <brett.deaton@gmail.com>
2024-02-25 21:52:03 -0800

    Add tests

---
+++ tests/test_draw.py
@@ -0,0 +1,25 @@
+"""Feature: Draw tones on a logarithmic spiral."""
+
+from semitone.chromatic import Chromatic
+from semitone.spiral_plot import SpiralPlot
+import unittest
+
+
+class TestDraw(unittest.TestCase):
+    """Feature tests for the SpiralPlot class."""
+
+    def test_see_all_tones_as_points_at_expected_radial_positions(self):
+        # TODO: silence the figure show (opening a browser tab)
+        scale = Chromatic("C")
+        points_expected = zip(
+            [pow(0.5, i/12) for i in range(12)],  # r
+            [30*i for i in range(12)]             # theta
+        )
+        fig = SpiralPlot.draw(scale)  # a tuple of plots
+        points_seen = zip(fig.data[0].r, fig.data[0].theta)
+        for i, (expected, seen) in enumerate(zip(points_expected, points_seen)):
+            self.assertAlmostEqual(expected[0], seen[0], places=5,
+                                   msg=f"Error in {i}th radius")
+            # mod 360, to normalize all angles to the range [0,360) deg
+            self.assertAlmostEqual(expected[1] % 360, seen[1] % 360, places=5,
+                                   msg=f"Error in {i}th angle")
---
+++ tests/test_scale.py
@@ -0,0 +1,30 @@
+"""Feature: Generate and manipulate sequences of tones aka scales."""
+
+# TODO make it so you only import Scale
+from semitone.chromatic import Chromatic
+from semitone.major import Major
+import unittest
+
+
+class TestScale(unittest.TestCase):
+    """Feature tests for the Scale class."""
+
+    def test_get_principle_and_primaries_as_floats(self):
+        scales_and_inits = [
+            (Chromatic, "C"),
+            (Major, "C"),
+        ]
+        for scale, init in scales_and_inits:
+            with self.subTest(scale=scale, initializer=init):
+                s = scale(init)
+                self.assertIsInstance(s.principle, float)
+                for tone in s.primaries:
+                    self.assertIsInstance(tone, float)
+
+    def test_compute_principle_of_equal_tempered_scales_as_expected(self):
+        principle = {"name": "C", "freq": 261.63}
+        for scale_type in (Chromatic, Major):
+            with self.subTest(scale_type=scale_type):
+                s = scale_type(principle["name"])
+                self.assertAlmostEqual(
+                    s.principle, principle["freq"], places=2)
