80a591cd844a43b02a6ccdb20d1aadcfdf6b0b4c
Brett Deaton <brett.deaton@gmail.com>
2024-04-21 17:56:26 -0700

    Add note name methods

--- semitone/chromatic.py
+++ semitone/chromatic.py
@@ -12,3 +12,9 @@ class Chromatic(EqualTempered):
         self.primaries = tuple(
             self.ith_freq_from_primary(self.principle, i) for i in range(12)
         )
+
+    def note_names(self) -> list[str]:
+        """Return 12 note names, rising, starting from the key principle"""
+        offset = EqualTempered.find_chromatic_index(self.key_name)
+        names_from_c = EqualTempered.note_names_including_enharmonics()
+        return names_from_c[offset:] + names_from_c[:offset]
--- semitone/equal_tempered.py
+++ semitone/equal_tempered.py
@@ -39,6 +39,17 @@ class EqualTempered(Scale):
         self.principle = self.freq_from_name(self.key_name)
 
     @staticmethod
+    def note_names_including_enharmonics() -> list[str]:
+        """Return the names of the traditional western notes starting from C
+
+        Returns:
+            the following list of length 12: ['C', 'Db/C#', 'D', ...]
+        """
+        return [
+            "/".join(enharmonics) for enharmonics in EqualTempered._SPELLINGS
+        ]
+
+    @staticmethod
     def ith_freq_from_primary(key_center: float, i: int) -> float:
         """Compute the frequency i semitones above the given key center.
 
@@ -65,14 +76,23 @@ class EqualTempered(Scale):
                 for example the 4th octave beginning at Middle C covers
                 approximately 262-523 Hz; default=4
         """
+        index = EqualTempered.find_chromatic_index(name)
+        freq_c4 = EqualTempered.ith_freq_from_primary(Scale._A440, -9)
+        frequencies = tuple(
+            EqualTempered.ith_freq_from_primary(freq_c4, i) for i in range(12)
+        )
+        return frequencies[index] * 2 ** (octave - 4)
+
+    @staticmethod
+    def find_chromatic_index(name: str) -> int:
+        """Lookup the index associated with a traditional western note name.
+
+        E.g. C is 0, C# and Db are 1, D is 2, ... B is 11
+        """
         for i, enharmonics in enumerate(EqualTempered._SPELLINGS):
             if name in enharmonics:
                 index = i
                 break
         else:
             raise ValueError(f"Name {name} is not an equal-tempered note name")
-        freq_c4 = EqualTempered.ith_freq_from_primary(Scale._A440, -9)
-        frequencies = tuple(
-            EqualTempered.ith_freq_from_primary(freq_c4, i) for i in range(12)
-        )
-        return frequencies[index] * 2 ** (octave - 4)
+        return index
