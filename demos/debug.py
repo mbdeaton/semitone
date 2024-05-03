a907ffef7566090bd808dda4b81cba2cff40cdf6
Brett Deaton <brett.deaton@gmail.com>
2024-05-02 23:22:35 -0700

    Add some more to the debugging script

--- demos/debug.py
+++ demos/debug.py
@@ -2,8 +2,10 @@
 
 from semitone.major import Major
 from semitone.spiral_plot import SpiralPlot
+from semitone.extender import Extender
 
+# This is intended to be scratch code to be modified for debugging purposes
 scale = Major("Bb")
 print(scale.primaries)
 fig = SpiralPlot.draw(scale)
-fig.write_image("test.png")
+print(Extender.extend(scale, octaves_below=0, octaves_above=0))
