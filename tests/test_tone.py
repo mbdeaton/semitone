f11f64b39e504d3da7bc279c127b47a9d4e31a51
Brett Deaton <brett.deaton@gmail.com>
2025-11-11 21:19:31 -0800

    Clarify tests

--- tests/test_tone.py
+++ tests/test_tone.py
@@ -1,32 +1,49 @@
-"""Feature: Compare tones with robustness against small floating point variation."""
+"""Feature: Compare tones robustly against small floating point variation."""
 
 import unittest
-import semitone.tone as tone
+from semitone.tone import Tone
 
 
 class TestTone(unittest.TestCase):
     """Feature tests for the Tone class."""
 
-    def test_compare_slightly_different_tones(self):
-        """Tones that differ by a tiny amount should compare as equal."""
-        base_frequency = 440.0
-        delta = 1e-10
-        jitter = 1e-16
-        base_tone = Tone(frequency=base_frequency)
-        tones_expected_same = (
-            Tone(frequency=base_frequency),
-            Tone(frequency=base_frequency - delta),
-            Tone(frequency=base_frequency + delta),
-        )
-        tones_expected_different = (
-            Tone(frequency=base_frequency - delta - jitter),
-            Tone(frequency=base_frequency + delta + jitter),
+    _delta_cents = 0.01  # allowable frequency difference in cents for equality
+    _jitter_cents = 1e-10  # extra amount to ensure equality/inequality
+    _base_freq = 440.0
+
+    def test_compare_slightly_different_tones_expect_equal(self):
+        """Tones differing less than accepted delta should compare equal."""
+
+        delta = (
+            self._base_freq * (self._delta_cents - self._jitter_cents) / 1200
         )
 
-        for tone in tones_expected_same:
-            with self.subTest(base_tone=base_tone, tone=tone):
-                self.assertEquals(base_tone, tone)
+        base_tone = Tone(self._base_freq)
 
-        for tone in tones_expected_different:
+        varying_tones = (
+            Tone(self._base_freq),
+            Tone(self._base_freq - delta),
+            Tone(self._base_freq + delta),
+        )
+
+        for tone in varying_tones:
             with self.subTest(base_tone=base_tone, tone=tone):
-                self.assertNotEquals(base_tone, tone)
+                self.assertEqual(base_tone, tone)
+
+    def test_compare_slightly_different_tones_expect_unequal(self):
+        """Tones differing more than accepted delta should compare unequal."""
+
+        delta = (
+            self._base_freq * (self._delta_cents + self._jitter_cents) / 1200
+        )
+
+        base_tone = Tone(self._base_freq)
+
+        varying_tones = (
+            Tone(self._base_freq - delta),
+            Tone(self._base_freq + delta),
+        )
+
+        for tone in varying_tones:
+            with self.subTest(base_tone=base_tone, tone=tone):
+                self.assertNotEqual(base_tone, tone)
