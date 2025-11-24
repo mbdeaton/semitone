7d43a55191f28473282a87dc33eb20ca2dd2a95c
Brett Deaton <brett.deaton@gmail.com>
2025-11-23 21:49:48 -0800

    Add Arbitrary Scale class

---
+++ semitone/arbitrary.py
@@ -0,0 +1,21 @@
+"""Arbitrary: a scale of any tones."""
+
+from semitone.tone import Tone
+from semitone.scale import Scale
+
+
+class Arbitrary(Scale):
+    """Tones of arbitrary frequencies."""
+
+    def __init__(self, frequencies: tuple[float, ...]) -> None:
+        """Initialize.
+
+        Args:
+            frequencies (tuple[float]): the frequencies in Hz,
+                will be sorted, with the lowest frequency as the principle
+        """
+        super().__init__()
+        self.principle = Tone(frequencies[0])
+        self.key_name = f"{self.principle.freq:.2f} Hz"
+        self.scale_name = "arb"
+        self.primaries = tuple(Tone(freq) for freq in sorted(frequencies))
