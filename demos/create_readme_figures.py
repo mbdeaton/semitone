e30646cfc00c6a3ad72353896725288209fb6e92
Brett Deaton <brett.deaton@gmail.com>
2026-03-13 20:49:15 -0700

    Smallify images and script

--- demos/create_readme_figures.py
+++ demos/create_readme_figures.py
@@ -5,9 +5,9 @@ import semitone as st
 
 def adjust_figure(figure):
     figure.update_layout(showlegend=False)
-    figure.update_layout(font=dict(size=24))
-    figure.update_traces(marker_size=10)
-    figure.update_layout(width=450, height=450)
+    figure.update_layout(font=dict(size=20))
+    figure.update_traces(marker_size=7)
+    figure.update_layout(width=360, height=310)
 
 
 scale = st.Chromatic("C").extend(0, 2)
